f76e6b0fc3fb33a8a34e42f11c34ce5d905e8487
SirDaywalker <93606588+SirDaywalker@users.noreply.github.com>
2023-06-01 18:10:58 +0200

    Client added
    lets goo

---
+++ ChatClient.py
@@ -0,0 +1,42 @@
+import socket
+from typing import Optional
+from datetime import datetime
+
+
+class ChatClient:
+    HOST: str
+    PORT: int
+
+    def __init__(self, host: str = "127.0.0.1", port: int = 80):
+        self.HOST = host
+        self.PORT = port
+
+    def _run_and_connect(self):
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            try:
+                s.connect((self.HOST, self.PORT))
+            except ConnectionRefusedError as e:
+                print(e)
+                return
+
+            print(f"Successfully connected to: {self.HOST} at {self.PORT}")
+
+            username: str = input("Enter your username:")
+            s.sendall(f"uID: {username + str(datetime.now())[21:26]}, username: {username}\n".encode())
+
+            data: Optional[bytes] = None
+            while data != "exit":
+                print(f"Other: {data}")
+                msg: str = input()
+                s.sendall(msg.encode())
+                data: bytes = s.recv(1024)
+
+    def _encrypt(self):
+        return
+
+    def _decrypt(self):
+        return
+
+
+c = ChatClient()
+c._run_and_connect()
