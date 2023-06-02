eab3d678c61ce4bca620a039deae32e899e4ba01
SirDaywalker <93606588+SirDaywalker@users.noreply.github.com>
2023-06-02 08:51:11 +0200

    added socket support

--- InputThread.py
+++ InputThread.py
@@ -1,13 +1,15 @@
 import threading
+import socket
 
 
 class KeyboardThread(threading.Thread):
 
-    def __init__(self, input_cbk=None, name='keyboard-input-thread'):
+    def __init__(self, input_cbk, s: socket):
         self.input_cbk = input_cbk
-        super(KeyboardThread, self).__init__(name=name)
+        self.s = s
+        super(KeyboardThread, self).__init__(name='keyboard-input-thread')
         self.start()
 
     def run(self):
         while True:
-            self.input_cbk(input())  # waits to get input + Return
+            self.input_cbk(input(), self.s)  # waits to get input + Return
